47f8b14b511d7daeec43dec30cb8bf9d54290699
camfeitosa <camilafeitosacoutinho@gmail.com>
2025-02-11 11:31:09 -0300

    Aula 1

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ import java.util.Scanner; // recebe input do usuário
 
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
         System.out.println("Hello World!"); // new line para ir para próx linha
         System.out.print("Teste");
         
@@ -14,7 +13,16 @@ public class Main {
         int soma;
 
         // Declaraçã do objeto Scanner
-        Scanner sc = new
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o primeiro número inteiro");
+        numero1 = sc.nextInt(); // nextLine -> string
+
+        System.out.println("Digite o segundo número inteiro");
+        numero2 = sc.nextInt();
+
+        soma = numero1 + numero2;
+
+        System.out.println("O valor da soma é: " + soma);
 
     }
 }
