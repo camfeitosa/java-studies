7e89dd8003cb0c63c01e62c72bb89e03a8379803
camfeitosa <camilafeitosacoutinho@gmail.com>
2025-02-11 11:18:20 -0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner; // recebe input do usuário
+
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello World!"); // new line para ir para próx linha
+        System.out.print("Teste");
+        
+        int numero1;
+        int numero2;
+        int soma;
+
+        // Declaraçã do objeto Scanner
+        Scanner sc = new
+
+    }
+}
