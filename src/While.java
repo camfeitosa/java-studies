3f71a64b765124ae06080d55610dc26615d5fc1c
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-06 11:20:14 -0300

    Aula while

---
+++ src/While.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class While {
+    public static void main(String[] args) {
+        int i = 0;
+
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while (i <= 5);
+    }
+}
